355f10eaee5224272a4721199975027bd8a9c790
athirahnaurah <fathirahnaurah@gmail.com>
2025-07-18 22:02:29 +0700

    init structure data

--- Tugas2.c
+++ Tugas2.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+struct Pegawai {
+    int nip;
+    char namaPegawai[50];
+    char alamat[100];
+    char noHp[13];
+    char jabatan[50];
+    char golongan[2];
+    int gaji;
+};
+
+int main(){
+
+}
